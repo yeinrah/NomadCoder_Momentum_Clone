c29cd19ea88be3eb67882b45aeb9b029fc62f487
Yein Rah <yein.rah@gmail.com>
2022-03-24 23:19:56 +0900

    weather

---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY = "aacf6cde8b00450e3b1975a84df3b21e";
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        });
+}
+function onGeoError() {
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
